a6c6de3807bdcb6a6964960b27d205c9485ec16d
MrkTheCoder <25789969+MrkTheCoder@users.noreply.github.com>
2025-10-18 08:47:25 +0330

    Add project files.

---
+++ EduTrack.App.Tests/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EduTrack.App.ViewModels;
+using FluentAssertions;
+
+namespace EduTrack.App.Tests.ViewModels
+{
+    public class MainWindowViewModelTests
+    {
+        [Fact]
+        public void Greeting_Should_Return_WelcomeMessage()
+        {
+            // Arrange
+            var vm = new MainWindowViewModel();
+
+            // Act
+            var result = vm.Greeting;
+
+            // Assert
+            result.Should().Be("Welcome to Avalonia!");
+        }
+    }
+}
---
+++ EduTrack.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EduTrack.App.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
