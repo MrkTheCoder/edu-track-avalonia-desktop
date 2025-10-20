5ee817a52824e3611c3445c1f0a1fcf4da147598
MrkTheCoder <25789969+MrkTheCoder@users.noreply.github.com>
2025-10-20 11:30:16 +0330

    Add initial EF Core migration
    - Added EduTrackDbContext and configured SQLite provider
    - Created DesignTimeDbContextFactory for design-time EF tools
    - Generated InitialCreate migration for Student and Course tables
    
    Changes:
    Added
    - EduTrack.Data/Context/EduTrackDbContext.cs
    - EduTrack.Data/Context/EduTrackDbContextFactory.cs
    - EduTrack.Data/Migrations/2025xxxxxx_InitialCreate.cs

---
+++ EduTrack.Data/Context/EduTrackDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EduTrack.Data.Context
+{
+    public class EduTrackDbContextFactory : IDesignTimeDbContextFactory<EduTrackDbContext>
+    {
+        public EduTrackDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EduTrackDbContext>();
+            optionsBuilder.UseSqlite("Data Source=EduTrack.db");
+
+            return new EduTrackDbContext(optionsBuilder.Options);
+        }
+    }
+}
