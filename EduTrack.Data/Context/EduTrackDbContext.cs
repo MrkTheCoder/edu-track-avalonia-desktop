9747b2e3df6ff098fb47bd1a7164d3a252a8bf4c
MrkTheCoder <25789969+MrkTheCoder@users.noreply.github.com>
2025-10-20 09:39:47 +0330

    Add EF Core setup and initial DbContext configuration
    Installed EF Core packages and created ApplicationDbContext
    to handle database operations for Student and Course entities using SQLite.
    
    Changes:
    Added
    - Microsoft.EntityFrameworkCore
    - Microsoft.EntityFrameworkCore.Sqlite
    - Microsoft.EntityFrameworkCore.Tools
    - ApplicationDbContext.cs in EduTrack.Data/Context

---
+++ EduTrack.Data/Context/EduTrackDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EduTrack.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduTrack.Data.Context
+{
+    // Class definition called: `Primary Constructor`
+    public class EduTrackDbContext(DbContextOptions<EduTrackDbContext> options) 
+        : DbContext(options)
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=EduTrack.db");
+            }
+        }
+    }
+}
