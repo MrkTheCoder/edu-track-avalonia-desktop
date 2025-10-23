b6cc44a9b26f86ba3514805bf3f9aa4a7b4cb2aa
MrkTheCoder <25789969+MrkTheCoder@users.noreply.github.com>
2025-10-23 11:53:55 +0330

    Add generic and entity repository interfaces
    Added a reusable generic IRepository<T> interface to define
    standard CRUD and query operations. Also created IStudentRepository and
    ICourseRepository interfaces inheriting from the generic base, preparing
    for future entity-specific extensions.
    
    Changes:
    Added:
     - IRepository<T>
     - IStudentRepository
     - ICourseRepository

---
+++ EduTrack.Core/Interfaces/ICourseRepository.cs
@@ -0,0 +1,8 @@
+﻿using EduTrack.Core.Models;
+
+namespace EduTrack.Core.Interfaces
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+    }
+}
---
+++ EduTrack.Core/Interfaces/IStudentRepository.cs
@@ -0,0 +1,8 @@
+﻿using EduTrack.Core.Models;
+
+namespace EduTrack.Core.Interfaces
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+    }
+}
