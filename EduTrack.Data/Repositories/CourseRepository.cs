6603993b649c914c3fa3498aad8ba564cb5eb999
MrkTheCoder <25789969+MrkTheCoder@users.noreply.github.com>
2025-10-26 08:05:00 +0330

    Implement concrete repository classes
    Added StudentRepository and CourseRepository classes to the
    Data layer. Both inherit from the GenericRepository<T> and implement their
    respective interfaces, preparing the foundation for data access operations.
    
    Changes:
    Added:
     - StudentRepository.cs
     - CourseRepository.cs

---
+++ EduTrack.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EduTrack.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduTrack.Data.Repositories
+{
+    public class CourseRepository(DbContext context)
+        :GenericRepository<Course>(context)
+    {
+    }
+}
---
+++ EduTrack.Data/Repositories/StudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using EduTrack.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduTrack.Data.Repositories
+{
+    public class StudentRepository(DbContext context) 
+        : GenericRepository<Student>(context)
+    {
+    }
+}
