11258654b77d02917ea1a5f36b0af8a9c7823011
MrkTheCoder <25789969+MrkTheCoder@users.noreply.github.com>
2025-10-26 07:40:11 +0330

    Add GenericRepository base implementation
    Introduced the GenericRepository<T> class in EduTrack.Data to
    provide reusable EF Core data access logic for all entities. Implements
    IRepository<T> with async CRUD operations and query support.
    
    Changes:
    Added:
     - GenericRepository<T> in EduTrack.Data/Repositories

---
+++ EduTrack.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq.Expressions;
+using EduTrack.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduTrack.Data.Repositories
+{
+    public class GenericRepository<T>(DbContext context) 
+        : IRepository<T> where T : class
+    {
+        protected readonly DbContext _context = context;
+        protected readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public async Task<T?> GetByIdAsync(int id)
+            => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await _dbSet.ToListAsync();
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) 
+            => await _dbSet.Where(predicate).ToListAsync();
+
+        public async Task AddAsync(T entity) 
+            => await _dbSet.AddAsync(entity);
+
+        public async Task AddRangeAsync(IEnumerable<T> entities) 
+            => await _dbSet.AddRangeAsync(entities);
+
+        public async Task RemoveAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await Task.CompletedTask;
+        }
+
+        public async Task RemoveAllAsync()
+        {
+            var allEntities = await GetAllAsync();
+            _dbSet.RemoveRange(allEntities);
+        }
+
+        public async Task RemoveByIdAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+                _dbSet.Remove(entity);
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await Task.CompletedTask;
+        }
+    }
+}
